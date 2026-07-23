ce807931a6c2f9a5f80db9ba251661f34125a023
jhazelton <36609942+jhazelton@users.noreply.github.com>
2026-07-23 00:54:10 -0400

    Refactor output block for environment URL and add AWS account

--- dev/outputs.tf
+++ dev/outputs.tf
@@ -1,3 +1,9 @@
 output "environment_url" {
   value = module.dev.environment_url
 }
+
+data "aws_caller_identity" "current" {}
+
+output "aws_account" {
+  value = data.aws_caller_identity.current.account_id
+}
