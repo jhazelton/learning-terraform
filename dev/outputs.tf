60fd37e2082eba7fb5f1211b054407cef717266a
jhazelton <36609942+jhazelton@users.noreply.github.com>
2026-07-23 00:59:19 -0400

    Refactor outputs and data blocks in outputs.tf

--- dev/outputs.tf
+++ dev/outputs.tf
@@ -2,14 +2,16 @@ output "environment_url" {
   value = module.dev.environment_url
 }
 
-data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {
 
-output "aws_account" {
-  value = data.aws_caller_identity.current.account_id
+  output "aws_account" {
+   value = data.aws_caller_identity.current.account_id
+  }
 }
 
-data "aws_region" "current" {}
+data "aws_region" "current" {
 
-output "current_region" {
-  value = data.aws_region.current.name
+  output "current_region" {
+    value = data.aws_region.current.name
+  }
 }
