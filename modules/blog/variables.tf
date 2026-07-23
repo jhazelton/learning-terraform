c06bc5945d44afb67157c867b9d3fbab874ad58a
jhazelton <36609942+jhazelton@users.noreply.github.com>
2026-07-23 00:45:19 -0400

    Change AMI filter owner to Amazon and update name
    Updated AMI filter to use Amazon's latest AMI naming convention.

--- modules/blog/variables.tf
+++ modules/blog/variables.tf
@@ -13,8 +13,8 @@ variable "ami_filter" {
   })
 
   default = {
-    name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
-    owner = "979382823631" # Bitnami
+    name  = "al2023-ami-2023*-x86_64"
+    owner = "amazon" # changed to amazon
   }
 }
 
