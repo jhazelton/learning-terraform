a6ae2d11f576948915ecb9553297177956ef6f96
jhazelton <36609942+jhazelton@users.noreply.github.com>
2026-07-24 16:38:35 -0400

    Downgrade blog_autoscaling module version to 7.0

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -32,7 +32,7 @@ module "blog_vpc" {
 
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "8.3.1"
+  version = "7.0"
 
   name                = "${var.environment.name}-blog"
   min_size            = var.asg_min
