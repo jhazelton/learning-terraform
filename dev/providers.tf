ee4713a7513b4299b3f12a87d0cde8551b9768fe
jhazelton <36609942+jhazelton@users.noreply.github.com>
2026-07-24 16:52:51 -0400

    Change AWS provider version to exact 5.84.0
    Updated AWS provider version constraint to be exact.

--- dev/providers.tf
+++ dev/providers.tf
@@ -1,13 +1,13 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.84.0" # <-- Forces Terraform to stay on Version 5
-    }
-  }
-}
+terraform { 
+  required_providers { 
+    aws = { 
+      source  = "hashicorp/aws" 
+      version = "5.84.0" # <-- Remove the ~> so it is EXACTLY 5.84.0
+    } 
+  } 
+} 
 
-provider "aws" {
-  region  = "us-east-1"
+provider "aws" { 
+  region = "us-east-1" 
 }
 
